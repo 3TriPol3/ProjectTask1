0f615ed7187def07f5459a06f831ea57ffe68c77
SystemAdminTkinter <gancster007@gmail.com>
2026-01-19 09:22:10 +0500

    ...

--- Connection/connect.py
+++ Connection/connect.py
@@ -1,8 +1,8 @@
 from peewee import *
 
 # Connect to a MySQL database on network.
-mysql_db = MySQLDatabase('rybin_g_task1',
-                         user='rybin_g_task1',
+mysql_db = MySQLDatabase('BessM82_g_task1',
+                         user='BessM82_g_task1',
                          password='111111',
                          host='10.11.13.118',
                          port=3306)
